b9553c2ee0e52235fa97f7fd9f83a8e674e0d864
MichaelJGryzz <michaeljgryschuk@gmail.com>
2024-05-29 04:07:24 -0400

    Fix code by correcting typo of "orignal" on "original.split()" function and replace non-existent method "reverseList()" with "reverse()"

--- reverse.js
+++ reverse.js
@@ -1,8 +1,20 @@
+// Provided Code with Error:
+//var input = process.argv[2];
+
+//if (input)
+  //console.log(reverse(input));
+
+//function reverse(original) {
+  //return orignal.split('').reverseList().join('');
+//}
+
+// Fixed Code:
 var input = process.argv[2];
 
 if (input)
   console.log(reverse(input));
 
 function reverse(original) {
-  return orignal.split('').reverseList().join('');
+  return original.split('').reverse().join('');// Spelt "original" wrong in the original.split() function
+  // There is no "reverseList()" method in JS
 }
