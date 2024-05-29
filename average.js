bcc707be7a9e964fbde56e30cc93b746dd57ab4b
MichaelJGryzz <michaeljgryschuk@gmail.com>
2024-05-29 03:56:14 -0400

    Fix provided code by adding missing curly bracket after "return sum / list.length;" line and swap out "++" for "+=" in the for...of loop.

--- average.js
+++ average.js
@@ -1,10 +1,24 @@
+// Provided Code with Errors:
+//function average(list) {
+    //var sum = 0;
+  
+    //for (var num of list) {
+      //sum ++ num;
+    //}
+  
+    //return sum / list.length;
+  
+//console.log(average([3, 5, 7]));
+
+// Fixed Code:
 function average(list) {
-    var sum = 0;
-  
-    for (var num of list) {
-      sum ++ num;
-    }
-  
-    return sum / list.length;
-  
+  var sum = 0;
+
+  for (var num of list) {
+    sum += num;// Used "++" instead of "+=" 
+  }
+
+  return sum / list.length;
+}// Forgot curly bracket
+
 console.log(average([3, 5, 7]));
